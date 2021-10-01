739a3e67c3a0442c20f49d665f8918643f52e864
Connor Shearer <connor_shearer@outlook.com>
2021-10-01 17:37:41 +0100

    No longer a need for a buffer since SharpZipLib handles that

--- src/unp4k/Program.cs
+++ src/unp4k/Program.cs
@@ -17,7 +17,6 @@ namespace unp4k
                 UseZip64 = UseZip64.Dynamic
             };
             pak.KeysRequired += (object sender, KeysRequiredEventArgs e) => e.Key = new byte[] { 0x5E, 0x7A, 0x20, 0x02, 0x30, 0x2E, 0xEB, 0x1A, 0x3B, 0xB6, 0x17, 0xC3, 0x0F, 0xDE, 0x1E, 0x47 };
-            byte[] buf = new byte[4096];
 
             foreach (ZipEntry entry in pak)
             {
