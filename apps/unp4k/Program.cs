55e0caa2cb1839bd0c0aae728252fa0c1a3bdb5b
Connor Shearer <connor_shearer@outlook.com>
2021-10-03 11:19:36 +0100

    After looking into it more, CIG almost certainly never use Zip64

--- apps/unp4k/Program.cs
+++ apps/unp4k/Program.cs
@@ -20,10 +20,7 @@ if (args.Length == 3 && !File.Exists(args[1]) && !Directory.Exists(args[1]))
 }
 
 using FileStream pakFile = File.OpenRead(args[0]);
-ZipFile pak = new(pakFile)
-{
-    UseZip64 = UseZip64.Dynamic
-};
+ZipFile pak = new(pakFile);
 pak.KeysRequired += (object sender, KeysRequiredEventArgs e) => e.Key = new byte[] { 0x5E, 0x7A, 0x20, 0x02, 0x30, 0x2E, 0xEB, 0x1A, 0x3B, 0xB6, 0x17, 0xC3, 0x0F, 0xDE, 0x1E, 0x47 };
 byte[] buf = new byte[4096];
 
