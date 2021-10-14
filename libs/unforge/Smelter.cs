2cd960c4ea1ff7f7cc19b9c6f9059c58748683c9
Connor Shearer <connor_shearer@outlook.com>
2021-10-14 06:59:49 +0100

    Make all launch settings use detailed mode and add Smelt launch settings

--- libs/unforge/Smelter.cs
+++ libs/unforge/Smelter.cs
@@ -1,45 +1,23 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Xml;
 
 namespace unforge
 {
 	public static class Smelter
 	{
-		private static bool _overwrite;
-		public static void Smelt(string path, bool overwrite = true)
+		public static void Smelt(FileInfo inFile, FileInfo outFile)
 		{
-			_overwrite = overwrite;
-			try
-			{
-				if (File.Exists(path))
-				{
-					if (Path.GetExtension(path) == ".dcb")
-					{
-						using BinaryReader br = new(File.OpenRead(path));
-						bool legacy = new FileInfo(path).Length < 0x0e2e00;
-						DataForge df = new(br, legacy);
-						df.Save(Path.ChangeExtension(path, "xml"));
-					}
-					else
-					{
-						if (!_overwrite)
-						{
-							if (!File.Exists(Path.ChangeExtension(path, "raw")))
-							{
-								File.Move(path, Path.ChangeExtension(path, "raw"));
-								path = Path.ChangeExtension(path, "raw");
-							}
-						}
-						XmlDocument xml = CryXmlSerializer.ReadFile(path);
-						if (xml != null) xml.Save(Path.ChangeExtension(path, "xml"));
-						else Logger.LogInfo($"{path} already in XML format");
-					}
-				}
+			Logger.LogInfo("");
+			if (inFile.Extension == ".dcb")
+            {
+				using BinaryReader br = new(inFile.Open(FileMode.Open, FileAccess.Read, FileShare.None));
+				new DataForge(br, inFile.Length < 0x0e2e00).Save(Path.ChangeExtension(outFile.FullName, "xml"));
 			}
-			catch (Exception e)
-			{
-				Logger.LogException(e);
+			else
+            {
+				XmlDocument xml = CryXmlSerializer.ReadFile(inFile.FullName);
+				if (xml != null) xml.Save(Path.ChangeExtension(outFile.FullName, "xml"));
+				else Logger.LogInfo($"{outFile.FullName} already in XML format");
 			}
 		}
 	}
