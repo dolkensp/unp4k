7e327c6dad82b9071550a94716d51a2323af730f
Connor Shearer <connor_shearer@outlook.com>
2021-10-16 11:50:25 +0100

    Add Linux support

---
+++ libs/unlib/OS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace unlib
+{
+    public static class OS
+    {
+        public static OSType Type { get; private set; }
+
+        public static bool IsWindows => Type == OSType.Windows;
+
+        public static bool IsLinux => Type == OSType.Linux;
+
+        public static bool IsAndroid => Type == OSType.Android;
+
+        public static bool IsMacOSX => Type == OSType.MacOSX;
+
+        public static bool IsiPhone => Type == OSType.iPhone;
+
+        static OS()
+        {
+            Type = (Environment.OSVersion.Platform != PlatformID.Win32NT) ? ((Environment.OSVersion.Platform == PlatformID.Unix) ? OSType.Linux : ((Environment.OSVersion.Platform == PlatformID.MacOSX) ? OSType.MacOSX : OSType.Windows)) : OSType.Windows;
+        }
+    }
+
+    public enum OSType
+    {
+        Windows,
+        Linux,
+        Android,
+        MacOSX,
+        iPhone
+    }
+}
