3b145f5a020c03b9136ab289056b87f3257db996
Connor Shearer <connor_shearer@outlook.com>
2021-10-24 16:32:07 +0100

    Do not make DataForge enumerable

--- libs/unforge/DataForge.cs
+++ libs/unforge/DataForge.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,7 +13,7 @@ namespace unforge
         public int RecordIndex { get; set; }
     }
 
-    public class DataForge //: IEnumerable
+    public class DataForge
 	{
         internal BinaryReader br;
         internal int FileVersion { get; set; }
