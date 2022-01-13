36567774a1ffb1a6036dc0f1ec87457e243b3c1e
Connor Shearer <connor_shearer@outlook.com>
2022-01-13 20:28:04 +0000

    Create MAUI unp4ked project

---
+++ apps/unp4ked/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace unp4ked
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ apps/unp4ked/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace unp4ked
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
