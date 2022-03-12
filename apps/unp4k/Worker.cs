f989cdb932f202bb88043775c02019da23e4a3af
Connor Shearer <connor_shearer@outlook.com>
2022-03-12 01:35:20 +0000

    Fix the input for ForgeData

--- apps/unp4k/Worker.cs
+++ apps/unp4k/Worker.cs
@@ -138,7 +138,7 @@ internal class Worker
                 FileInfo smeltedFile = new(Path.Join(Globals.SmelterOutDirectory.FullName, entry.Name));
                 try
                 {
-                    if (extractedFile.Extension is ".dcb") await DataForge.ForgeData(new(extractedFile, smeltedFile), Globals.DetailedLogs);
+                    if (extractedFile.Extension is ".dcb") await DataForge.ForgeData(extractedFile, smeltedFile, Globals.DetailedLogs);
                     else DataForge.SerialiseData(extractedFile, smeltedFile);
                 }
                 catch (Exception e)
