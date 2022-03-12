b143ec46624b2bcc375c9f13abe620218913cfa4
Connor Shearer <connor_shearer@outlook.com>
2022-03-12 01:54:08 +0000

    I see why this was done now

--- libs/unforge/DataForgeIndex.cs
+++ libs/unforge/DataForgeIndex.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Xml;
 
 namespace unforge;
@@ -49,7 +51,7 @@ internal class DataForgeIndex
             else
             {
                 List<U> o = new();
-                for (int i = 0; i < size; i++) o.Add((U)Activator.CreateInstance(typeof(U), this));
+                for (int i = 0; i < size; i++) o.Add((U)typeof(U).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic).First().Invoke(new object[] { this }));
                 return o;
             }
         }
@@ -61,35 +63,64 @@ internal class DataForgeIndex
 
         for (int i = 0; i < 4; i++) Reader.ReadUInt16(); // Offset - TODO: Figure out what this is - This might be a Int64?
 
-        StructDefinitionTable =         ReadArray<DataForgeStructDefinition>(Reader.ReadInt32());
-        PropertyDefinitionTable =       ReadArray<DataForgePropertyDefinition>(Reader.ReadInt32());
-        EnumDefinitionTable =           ReadArray<DataForgeEnumDefinition>(Reader.ReadInt32());
-        DataMappingTable =              ReadArray<DataForgeDataMapping>(Reader.ReadInt32());
-        RecordDefinitionTable =         ReadArray<DataForgeRecord>(Reader.ReadInt32());
+        int structDefinitionCount =     Reader.ReadInt32();
+        int propertyDefinitionCount =   Reader.ReadInt32();
+        int enumDefinitionCount =       Reader.ReadInt32();
+        int dataMappingCount =          Reader.ReadInt32();
+        int recordDefinitionCount =     Reader.ReadInt32();
 
-        Int8Values =                    ReadArray<DataForgeInt8>(Reader.ReadInt32());
-        Int16Values =                   ReadArray<DataForgeInt16>(Reader.ReadInt32());
-        Int32Values =                   ReadArray<DataForgeInt32>(Reader.ReadInt32());
-        Int64Values =                   ReadArray<DataForgeInt64>(Reader.ReadInt32());
-        UInt8Values =                   ReadArray<DataForgeUInt8>(Reader.ReadInt32());
-        UInt16Values =                  ReadArray<DataForgeUInt16>(Reader.ReadInt32());
-        UInt32Values =                  ReadArray<DataForgeUInt32>(Reader.ReadInt32());
-        UInt64Values =                  ReadArray<DataForgeUInt64>(Reader.ReadInt32());
-        BooleanValues =                 ReadArray<DataForgeBoolean>(Reader.ReadInt32());
-        SingleValues =                  ReadArray<DataForgeSingle>(Reader.ReadInt32());
-        DoubleValues =                  ReadArray<DataForgeDouble>(Reader.ReadInt32());
-        GuidValues =                    ReadArray<DataForgeGuid>(Reader.ReadInt32());
-        StringValues =                  ReadArray<DataForgeStringLookup>(Reader.ReadInt32());
-        LocaleValues =                  ReadArray<DataForgeLocale>(Reader.ReadInt32());
-        EnumValues =                    ReadArray<DataForgeEnum>(Reader.ReadInt32());
-        StrongValues =                  ReadArray<DataForgePointer>(Reader.ReadInt32());
-        WeakValues =                    ReadArray<DataForgePointer>(Reader.ReadInt32());
+        int booleanValueCount =         Reader.ReadInt32();
+        int int8ValueCount =            Reader.ReadInt32();
+        int int16ValueCount =           Reader.ReadInt32();
+        int int32ValueCount =           Reader.ReadInt32();
+        int int64ValueCount =           Reader.ReadInt32();
+        int uint8ValueCount =           Reader.ReadInt32();
+        int uint16ValueCount =          Reader.ReadInt32();
+        int uint32ValueCount =          Reader.ReadInt32();
+        int uint64ValueCount =          Reader.ReadInt32();
 
-        ReferenceValues =               ReadArray<DataForgeReference>(Reader.ReadInt32());
-        EnumOptionTable =               ReadArray<DataForgeStringLookup>(Reader.ReadInt32());
+        int singleValueCount =          Reader.ReadInt32();
+        int doubleValueCount =          Reader.ReadInt32();
+        int guidValueCount =            Reader.ReadInt32();
+        int stringValueCount =          Reader.ReadInt32();
+        int localeValueCount =          Reader.ReadInt32();
+        int enumValueCount =            Reader.ReadInt32();
+        int strongValueCount =          Reader.ReadInt32();
+        int weakValueCount =            Reader.ReadInt32();
 
-        uint textLength = Reader.ReadUInt32();
+        int referenceValueCount =       Reader.ReadInt32();
+        int enumOptionCount =           Reader.ReadInt32();
+
+        uint textLength =               Reader.ReadUInt32();
         Reader.ReadUInt32(); // Offset - TODO: Figure out what this is
+
+        StructDefinitionTable =         ReadArray<DataForgeStructDefinition>(structDefinitionCount);
+        PropertyDefinitionTable =       ReadArray<DataForgePropertyDefinition>(propertyDefinitionCount);
+        EnumDefinitionTable =           ReadArray<DataForgeEnumDefinition>(enumDefinitionCount);
+        DataMappingTable =              ReadArray<DataForgeDataMapping>(dataMappingCount);
+        RecordDefinitionTable =         ReadArray<DataForgeRecord>(recordDefinitionCount);
+
+        BooleanValues =                 ReadArray<DataForgeBoolean>(booleanValueCount);
+        Int8Values =                    ReadArray<DataForgeInt8>(int8ValueCount);
+        Int16Values =                   ReadArray<DataForgeInt16>(int16ValueCount);
+        Int32Values =                   ReadArray<DataForgeInt32>(int32ValueCount);
+        Int64Values =                   ReadArray<DataForgeInt64>(int64ValueCount);
+        UInt8Values =                   ReadArray<DataForgeUInt8>(uint8ValueCount);
+        UInt16Values =                  ReadArray<DataForgeUInt16>(uint16ValueCount);
+        UInt32Values =                  ReadArray<DataForgeUInt32>(uint32ValueCount);
+        UInt64Values =                  ReadArray<DataForgeUInt64>(uint64ValueCount);
+
+        SingleValues =                  ReadArray<DataForgeSingle>(singleValueCount);
+        DoubleValues =                  ReadArray<DataForgeDouble>(doubleValueCount);
+        GuidValues =                    ReadArray<DataForgeGuid>(guidValueCount);
+        StringValues =                  ReadArray<DataForgeStringLookup>(stringValueCount);
+        LocaleValues =                  ReadArray<DataForgeLocale>(localeValueCount);
+        EnumValues =                    ReadArray<DataForgeEnum>(enumValueCount);
+        StrongValues =                  ReadArray<DataForgePointer>(strongValueCount);
+        WeakValues =                    ReadArray<DataForgePointer>(weakValueCount);
+
+        ReferenceValues =               ReadArray<DataForgeReference>(referenceValueCount);
+        EnumOptionTable =               ReadArray<DataForgeStringLookup>(enumOptionCount);
 
         List<DataForgeString> buffer = new();
         long maxPosition = Reader.BaseStream.Position + textLength;
