e8883426f94f64d227edaa036115da91c1244df6
Connor Shearer <connor_shearer@outlook.com>
2022-04-29 16:26:14 +0100

    Create the MAUI Blazor GUI project

---
+++ apps/unp4k.GUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace unp4k.GUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ apps/unp4k.GUI/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using unp4k.GUI.Data;
+
+namespace unp4k.GUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
